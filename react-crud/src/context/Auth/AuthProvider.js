77c22b8c09809847da9b98ae352f256bfae6b430
Guilherme Helton <guilhermehelto@gmail.com>
2022-04-09 10:15:05 -0300

    refactor

--- react-crud/src/context/Auth/AuthProvider.js
+++ react-crud/src/context/Auth/AuthProvider.js
@@ -25,7 +25,7 @@ export const AuthProvider = ({ children }) => {
     }
 
     return (
-        <AuthContext.Provider value={{ user, login, register, logout }}>
+        <AuthContext.Provider value={{ user, login, register }}>
             {children}
         </AuthContext.Provider>
     )
